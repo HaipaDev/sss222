4e49cda7d135fa8d1cbdfea01128052b23f1e549
HyperGamesDev <hypergamesdev@gmail.com>
2021-04-11 14:09:46 +0200

    Launcher & Latest PreRelease buttons
    +Fixed variable writing

--- docs/githubapi.js
+++ docs/githubapi.js
@@ -1,17 +1,62 @@
-var api_url="https://api.github.com/repos/hypergamesdev/sss222/releases/latest";
-var data;
-var name;
-async function getapi(url){
-	const response=await fetch(url);
-	data=await response.json();
-	//console.log(data);
-	name=data.name;
+var releasesUrl="https://api.github.com/repos/hypergamesdev/sss222/releases";
+var releasesData;
+var releasesDataJSON;
+var stableUrl="https://api.github.com/repos/hypergamesdev/sss222/releases/latest";
+var stableUrlGoto="https://github.com/hypergamesdev/sss222/releases/latest";
+var stableData;
+var stableTag;
+var stableName;
+var latestUrlGoto="https://github.com/HyperGamesDev/sss222/releases/tag/";
+var latestData;
+var latestTag;
+var latestName;
+var launcherUrl="https://github.com/HyperGamesDev/sss222/releases/download/v0.3-beta/SSS222-Launcher-11.08.rar";
+async function getapiReleases(){
+	const responseReleases=await fetch(releasesUrl);
+	releasesData=await responseReleases.json();
+	
+	const responseStable=await fetch(stableUrl);
+	stableData=await responseStable.json();
+	
+	console.log("Releases Data");
+	console.log(releasesData);
+	latestData=releasesData[0];
+	console.log("Latest Data");
+	console.log(latestData);
+	latestTag=latestData.tag_name;
+	latestName=(latestData.name).replace("SSS222","");
+	
+	console.log("Stable Data");
+	console.log(stableData);
+	stableName=(stableData.name).replace("SSS222","");
+	
+	//sleep(100);
+	setReleasesText();
 }
-getapi(api_url);
+async function getapiStable(){
+	const responseStable=await fetch(stableUrl);
+	stableData=await responseStable.json();
+	console.log("Stable Data");
+	console.log(stableData);
+	stableName=(stableData.name).replace("SSS222","");
+	
+}
+getapiReleases();
 
-function getLatestReleaseName(){
-	name=name.replace("SSS222","");
-	return name;
+function setReleasesText(){
+	document.getElementById("stableText").innerHTML=stableName;
+	document.getElementById("latestText").innerHTML=latestName;
+}
+
+function downloadLauncher(){
+	location.href=launcherUrl;
+}
+function goToStableRelease(){
+	location.href=stableUrlGoto;
+}
+function goToLatestRelease(){
+	latestUrlGoto=latestUrlGoto.concat(latestTag);
+	location.href=latestUrlGoto;
 }
 
 function sleep(milliseconds) {
